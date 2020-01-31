5f350e02b36d6989670bc9d7b1a9a382c4256d7f
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-01-31 14:50:15 -0500

    1.2.3 Formatting

--- main.rs
+++ main.rs
@@ -1,21 +1,51 @@
-use std::fmt;
+use std::fmt::{self, Formatter,Display};
 
-struct List(Vec<i32>);
+struct City {
+    name : &'static str,
+    lat: f32,
+    lon : f32,
+}
+impl Display for City {
+    fn fmt(&self, f: &mut Formatter) ->fmt::Result {
+        let lat_c = if self.lat >=0.0 {'N' } else {'S'};
+        let lon_c = if self.lon >=0.0 { 'E' } else {'W'};
 
-impl  fmt::Display for List {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let vec = &self.0;
-        write!(f,"[")?;
-
-        for (count, v) in vec.iter().enumerate() {
-            if count !=0 { write!(f, ", ")?;}
-            write!(f,"{}",v)?;
-        }
-        write!(f, "]")
+        write!(f, "{}: {:.3}°{} {:.3}°{}",
+            self.name, self.lat.abs(),lat_c, self.lon.abs(),lon_c)
     }
 }
 
-fn main() {
-    let v = List(vec![1,2,3]);
-    println!("{}", v);
+#[derive(Debug)]
+struct Color {
+    red:u8,
+    green:u8,
+    blue:u8,
 }
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter)  ->fmt::Result {
+        write!(f, "RGB ({R}, {G}, {B}) 0x{:02X}{:02X}{:02X}",
+        R = &self.red, G = &self.green, B = &self.blue
+        )
+    }
+}
+
+fn main (){
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display.
+        println!("{}", *color);
+    }
+}
+
