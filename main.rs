eb96155484fe1faf23fe44c6e197b7de211f91c4
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-02-04 14:21:27 -0500

    literals and operators

--- main.rs
+++ main.rs
@@ -1,25 +1,16 @@
 fn main() {
-    // Variables can be type annotated.
-    let logical: bool = true;
+    // println!("1 + 2 = {}", 1u32 + 2);
+    // println!("1 - 2 = {}", 1i32 - 2);
 
-    let a_float: f64 = 1.0; // Regular annotation
-    let an_integer = 5i32; // Suffix annotation
+    // println!("true AND false is {}", true && false);
+    // println!("true OR false is {}", true || false);
+    // println!("NOT true is {}", !true);
 
-    // Or a default will be used.
-    let default_float = 3.0; // `f64`
-    let default_integer = 7; // `i32`
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {:b}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:b}", 0x80u32 >> 2);
 
-    // A type can also be inferred from context
-    let mut inferred_type = 12; // Type i64 is inferred from another line
-    inferred_type = 4294967296i64;
-
-    // A mutable variable's value can be changed.
-    let mut mutable = 12; // Mutable `i32`
-    mutable = 21;
-
-    // Error! The type of a variable can't be changed.
-    mutable = true;
-
-    // Variables can be overwritten with shadowing.
-    let mutable = true;
+    println!("One million is written as {}", 1_000_000u32);
 }
