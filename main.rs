100f095d5159efd55bb0c898545f3a4ddfa130f8
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-02-11 15:44:08 -0500

    2.2 tuples done

--- main.rs
+++ main.rs
@@ -1,16 +1,45 @@
+fn reverse(pair :(i32, bool)) -> (bool, i32) {
+    let (integer, boolean) = pair;
+    (boolean , integer)
+}
+
+#[derive(Debug)]
+struct Matrix (f32, f32, f32, f32);
+
 fn main() {
-    // println!("1 + 2 = {}", 1u32 + 2);
-    // println!("1 - 2 = {}", 1i32 - 2);
+    let long_tuple = (1u8, 2u16, 3u32, 4u64,
+        -1i8, -2i16, -3i32, -4i64,
+        0.1f32, 0.2f64,
+        'a', true);
 
-    // println!("true AND false is {}", true && false);
-    // println!("true OR false is {}", true || false);
-    // println!("NOT true is {}", !true);
+    println!(" long tuple first value :{}", long_tuple.0);
+    println!(" long tuple second value : {}", long_tuple.1);
+    
+    let tuple_of_tuple = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
 
-    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
-    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
-    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
-    println!("1 << 5 is {:b}", 1u32 << 5);
-    println!("0x80 >> 2 is 0x{:b}", 0x80u32 >> 2);
+    println!("tuple of tuples : {:?}", tuple_of_tuple);
 
-    println!("One million is written as {}", 1_000_000u32);
+     // But long Tuples cannot be printed
+    // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    // println!("too long tuple: {:?}", too_long_tuple);
+    // TODO ^ Uncomment the above 2 lines to see the compiler error
+    
+    let pair = (1, true);
+    println!("pair is {:?}", pair);
+
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    // To create one element tuples, the comma is required to tell them apart
+    // from a literal surrounded by parentheses
+    println!("one element tuple: {:?}", (5u32,));
+    println!("just an integer: {:?}", (5u32));
+
+    //tuples can be destructured to create bindings
+    let tuple = (1, "hello", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
 }
